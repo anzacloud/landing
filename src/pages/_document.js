355a2da2195c5e377339a1731732330cf0cff94f
anzacloud <anzacloud@gmail.com>
2020-11-23 00:13:14 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,6 +17,41 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Coiny&display=swap"
             rel="stylesheet"
           />
+          {/* <!-- Chrome, Firefox OS and Opera --> */}
+          <meta name="theme-color" content="#3729BE" />
+          {/* <!-- Windows Phone --> */}
+          <meta name="msapplication-navbutton-color" content="#3729BE" />
+          {/* <!-- Primary Meta Tags --> */}
+          <title>Anza Cloud - Begin Cloud Migration</title>
+          <meta name="title" content="Anza Cloud - Begin Cloud Migration" />
+          <meta
+            name="description"
+            content="Anza Cloud is focussed on helping businesses transform to cloud enterprises. We provide tools that help businesses keep up with the rapidly changing technology."
+          />
+          {/* <!-- Open Graph / Facebook --> */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://anza.cloud" />
+          <meta
+            property="og:title"
+            content="Anza Cloud - Begin Cloud Migration"
+          />
+          <meta
+            property="og:description"
+            content="Anza Cloud is focussed on helping businesses transform to cloud enterprises. We provide tools that help businesses keep up with the rapidly changing technology."
+          />
+          <meta property="og:image" content="assets/images/favicon.png" />
+          {/* <!-- Twitter --> */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://anza.cloud" />
+          <meta
+            property="twitter:title"
+            content="Anza Cloud - Begin Cloud Migration"
+          />
+          <meta
+            property="twitter:description"
+            content="Anza Cloud is focussed on helping businesses transform to cloud enterprises. We provide tools that help businesses keep up with the rapidly changing technology."
+          />
+          <meta property="twitter:image" content={favicon} />
         </Head>
         <body>
           <script
